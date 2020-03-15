ff8999ee33e2c782385bb27f00b71dca11fe758a
Derivativee <panosgenre@gmail.com>
2020-03-15 11:56:37 +0200

    test.cpp was added

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main()
+{
+	cout<<"Max Long Long Int: "<<numeric_limits<char>::max()<<" Min Long Long Int: "<<numeric_limits<char>::min()<<endl;
+}
